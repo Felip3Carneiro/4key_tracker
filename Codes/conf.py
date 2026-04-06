4874fec05b5af1e4450ebdafb0fc7d22ecb40246
Felipe Carneiro <felipevilarcarneiro@outlook.com>
2026-04-06 14:05:43 -0300

    Nice update

--- Codes/conf.py
+++ Codes/conf.py
@@ -1,4 +1,8 @@
-import os
+import os, sys
+from rich.console import Console
+from rich.panel import Panel
+from rich.table import Table
+from PIL import Image
 
 def vel(height, skin):
     if skin == "circ0":
@@ -14,12 +18,53 @@ def vel(height, skin):
         if height < 70:
             return 15
         
-def skins():
+def skins(skin_0, skin_1, background):
     note_skins = []
+    note_xy = []
+
     back_skins = []
+    back_xy = []
+
+    #Organização
     for file in os.listdir("images"):
         if "0" in file or "1" in file:
             note_skins.append(file[:-4])
+            with Image.open(f"Images/{file}") as img:
+                note_xy.append(img.size)  # (largura, altura)
         else:
             back_skins.append(file[:-4])
-    return [note_skins, back_skins]
+            with Image.open(f"Images/{file}") as img:
+                back_xy.append(img.size)  # (largura, altura)
+
+    #A parte daora
+    console = Console()
+
+    #Notes
+    table_n = Table(title="[underline blue]Note Skins[/underline blue]")
+    table_n.add_column("Name")
+    table_n.add_column("Size")
+
+    for name, size in zip(note_skins, note_xy):
+        table_n.add_row(name, f"{size[0]}x{size[1]}")
+
+    #Backgrounds
+    table_b = Table(title="[underline blue]Note Skins[/underline blue]")
+    table_b.add_column("Name")
+    table_b.add_column("Size")
+    
+    for name, size in zip(back_skins, back_xy):
+        table_b.add_row(name, f"{size[0]}x{size[1]}")
+
+    #Print final
+    console.print(table_n)
+    console.print(table_b)
+
+    if skin_0 and skin_1 in note_skins and background in back_skins:
+        console.print("\n[bold green][underline green]All skins found![/]")
+    else:
+        error()
+
+def error():
+    console = Console()
+    console.print(Panel("[bold red][ERROR]Nome do arquivo não existe, tente novamente[/bold red]", title="Aviso"))
+    sys.exit()
